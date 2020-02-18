a381132ac977551a5092b22ce8326ae8e266036b
nssliu <825559615@qq.com>
2020-02-18 10:59:48 +0800

    'boss'

--- src/main/java/com/fullproject/user/AddClass.java
+++ src/main/java/com/fullproject/user/AddClass.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author liuzhiheng
  * @version 1.0
  * @date 2020/2/18 9:58
- * @describe:我已经修改过了
+ * @describe:我已经修改过了，我是老大，我已经看到了。
  */
 @RestController
 @RequestMapping("/addClass")
