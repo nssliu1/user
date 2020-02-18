da571d98bd0dcc6a055710a34e7633b73c0d2a05
nssliu1 <61137580+nssliu1@users.noreply.github.com>
2020-02-18 10:35:09 +0800

    Update AddClass.java

--- src/main/java/com/fullproject/user/AddClass.java
+++ src/main/java/com/fullproject/user/AddClass.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author liuzhiheng
  * @version 1.0
  * @date 2020/2/18 9:58
- * @describe:
+ * @describe:我已经修改过了
  */
 @RestController
 @RequestMapping("/addClass")
